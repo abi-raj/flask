2cd5d6c81a8d29a227bf06f00c4b20cae1230100
Abiraj3011 <abiraj.rajendran@zoho.com>
2020-06-12 10:25:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,12 @@ def api():
     srch="search.php?req="+query+"&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
     srch_url=url+srch
     return getbooklinks(srch_url)
+
 def getbooklinks(srch_url):
-   return str(srch_url)     
+    cont=requests.get(str(srch_url)).content
+    bcont=bs(cont,'html.parser')
+    return str(bcont)
+
+        
 if __name__ == '__main__':
     app.run()
