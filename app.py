e24557afcadccbf8e99f93e8fbc61bc1ca9a24f8
Abiraj3011 <abiraj.rajendran@zoho.com>
2020-06-12 09:31:12 +0530

    make it better

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/api',methods=['GET'])
+@app.route('/api')
 
 def hello_world():
     url="http://gen.lib.rus.ec/"
-    query = str(request.args['query'])
-    srch="search.php?req="+query+"&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
-    srch_url=url+srch
+    #query = str(request.args['query'])
+    #srch="search.php?req="+query+"&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
+    #srch_url=url+srch
     #hdrs = {'User-Agent': 'Mozilla / 5.0 (X11 Linux x86_64) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 52.0.2743.116 Safari / 537.36'}
-    resp = requests.get(srch_url).content
+    resp = requests.get(url).content
     bsa=bs(resp,'html.parser')
- 
     return str(bsa)
 if __name__ == '__main__':
     app.run()
