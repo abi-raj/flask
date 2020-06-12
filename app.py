2cae699cc70cc2dc79a2cde6772bc48c238e2489
Abiraj3011 <abiraj.rajendran@zoho.com>
2020-06-12 10:15:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 
 app = Flask(__name__)
 
-def getbooklinks(srch_url):
-   return str(srch_url) 
+#def getbooklinks(srch_url):
+#   return str(srch_url) 
 
 @app.route('/api',methods=['GET'])
 def api():
@@ -19,6 +19,6 @@ def api():
         pass
     srch="search.php?req="+query+"&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
     srch_url=url+srch
-    return getbooklinks(srch_url)
+    return str(srch_url)
 if __name__ == '__main__':
     app.run()
