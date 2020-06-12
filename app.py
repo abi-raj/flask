f5882816a40d5769aa573a7e68c6c325849b94d1
Abiraj3011 <abiraj.rajendran@zoho.com>
2020-06-12 09:33:16 +0530

    make it better

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 
 def hello_world():
     url="http://gen.lib.rus.ec/"
-    #query = str(request.args['query'])
-    #srch="search.php?req="+query+"&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
-    #srch_url=url+srch
+    query = str(request.args['query'])
+    srch="search.php?req="+query+"&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column=def"
+    srch_url=url+srch
     #hdrs = {'User-Agent': 'Mozilla / 5.0 (X11 Linux x86_64) AppleWebKit / 537.36 (KHTML, like Gecko) Chrome / 52.0.2743.116 Safari / 537.36'}
-    resp = requests.get(url).content
+    resp = requests.get(srch_url).content
     bsa=bs(resp,'html.parser')
     return str(bsa)
 if __name__ == '__main__':
