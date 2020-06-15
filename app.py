874945a3fd30e6a1cb558b580323d0c591ba1586
Abiraj3011 <abiraj.rajendran@zoho.com>
2020-06-15 16:22:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,18 +39,18 @@ def getbooklinks(srch_url):
                     btitle=btitle.replace("-"," ")
                     btitle=btitle.replace("  ","")
                     
-                #if b==3:
-                  #  bpub=g.text
-                   # if len(bpub)==0:
-                 #       bpub='NA'
+                if b==3:
+                    bpub=g.text
+                    if len(bpub)==0:
+                        bpub='NA'
                 if b==4:
                     byear=g.text
-               # if b==5:
-                 #   bpages=g.text
+                if b==5:
+                    bpages=g.text
                 if b==6:
                     blan=g.text
-                #if b==7:
-                 #   bsize=g.text
+                if b==7:
+                    bsize=g.text
                 if b==8:
                     bformat=g.text
                 #9th 'td' has the LINK
@@ -61,14 +61,14 @@ def getbooklinks(srch_url):
                         c['index']=len(l)
                         c['Arthor']=arthr
                         c['link']=h['href']
-                        dd=c['link']
-                        c['image']= img(dd)
+                        #dd=c['link']
+                        #c['image']= img(dd)
                         c['Title']=btitle
-                        #c['Publisher']=bpub
+                        c['Publisher']=bpub
                         c['Year']=byear
-                        #c['Total Pages']=bpages
+                        c['Total Pages']=bpages
                         c['Language']=blan
-                        #c['Size']=bsize
+                        c['Size']=bsize
                         c['Format']=bformat
                         #print(c)
                         l.append(c)
@@ -82,20 +82,41 @@ def getbooklinks(srch_url):
     data['data']=l
     return jsonify(data)
 
-def img(link):
+@app.route('/book',methods=['GET'])
+def book():
+    link = str(request.args['link'])
+    lis=[]
     icnt=0
-    cont=rq.get(str(link)).content
+    b=0
+    cont=rq.get(link).content
     bcont=bs(cont,'html.parser')
+    
     for i in bcont.find_all('div'):
-        
         if icnt == 0:
-            
             for j in i.find_all('img',src=True):
                 imgli="http://93.174.95.29"+j['src']
-                
             break
-    return str(imgli)
 
+    c=0
+    for w in bcont.find_all('p'):
+        if c==2:
+            isbn=w.text
+            break
+        c=c+1
+    for a in bcont.find_all('td'):
+        if b==1:
+            for ab in (a.find_all('h2')):
+                for abc in ab.find_all('a',href=True):
+                    fileLink={}
+                    fileLink['File']=abc['href']
+                    fileLink['Image']=imgli
+                    fileLink['ISBN']=isbn
+                    lis.append(fileLink)
+            break
+        b=b+1
+    book={}
+    book["data"]=lis
+    return jsonify(book)
 
 if __name__ == "__main__":
     app.run()
